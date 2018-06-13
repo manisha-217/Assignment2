13779926a8e7178fd7071a51a79311f06036e40f
manisha-217 <manishakumari993424@gmail.com>
2018-06-12 23:52:34 -0700

    python print is upload

---
+++ correction.py
@@ -0,0 +1,3 @@
+name=("tony stak")
+salary=int(100000)
+print("%s,%d"%(name,salary))
---
+++ input.py
@@ -0,0 +1,4 @@
+x=int(input("enter anumber"))
+y=int(input("enter another number"))
+z=int(input("enter third number"))
+print(x,y,z)
